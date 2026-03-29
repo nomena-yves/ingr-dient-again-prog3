249d7cc72f8e3cef24589340a925bdce87000665
nomena <hei.nomenjanahary@gmail.com>
2026-03-29 03:34:58 +0300

    first commit

---
+++ src/main/java/hei/group/ingredientagain/IngredientAgainApplication.java
@@ -0,0 +1,13 @@
+package hei.group.ingredientagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientAgainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/group/ingredientagain/IngredientAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.group.ingredientagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
