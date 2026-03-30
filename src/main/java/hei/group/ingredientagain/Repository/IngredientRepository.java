5e89c743503875c8944bb3ddc7480c8af3350c5c
nomena <hei.nomenjanahary@gmail.com>
2026-03-30 21:28:11 +0300

    finition de la methode findByIngredient

--- src/main/java/hei/group/ingredientagain/Repository/IngredientRepository.java
+++ src/main/java/hei/group/ingredientagain/Repository/IngredientRepository.java
@@ -1,5 +1,6 @@
 package hei.group.ingredientagain.Repository;
 
+import hei.group.ingredientagain.Entity.DishEntity;
 import hei.group.ingredientagain.Entity.IngredientEntity;
 import hei.group.ingredientagain.Mapping.DishMapper;
 import hei.group.ingredientagain.Mapping.IngredientMapper;
@@ -29,6 +30,7 @@ public class IngredientRepository {
         int offset = (page - 1) * size;
         String sql = "select i.id,i.name,i.price,i.category,di.id_dish,di.id_ingredient,di.id_dish,d.name as name_ingredient,d.price as dish_price,d.dish_type from ingredient i inner join dishingredient di on i.id=di.id_ingredient inner join  dish d on di.id_dish=d.id limit ? offset ? ";
         List<IngredientEntity> Listingredients = new ArrayList<>();
+        DishEntity dishEntity = null;
         try(Connection conn = dataSource.getConnection()){
 PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setInt(1, offset);
@@ -36,9 +38,16 @@ PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
                 ingredientMapper.map(rs);
-
+            if (ingredientMapper.map(rs).getDish()!=null){
+                IngredientEntity ingredient = ingredientMapper.map(rs);
+                ingredient.setDish(dishMapper.map(rs));
+                Listingredients.add(ingredient);
             }
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
         }
+        return Listingredients;
     }
 
 }
